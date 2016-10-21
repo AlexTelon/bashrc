93c6be6d308a4cef3c44274713cbfb1c9955b956
Alex Telon <alex.telon@gmail.com>
2016-10-21 20:10:39 +0200

    font fix, small update to i3 config and setupi3

--- setupi3.sh
+++ setupi3.sh
@@ -4,6 +4,12 @@ echo "Only setting up some of the files."
 cp -ir .i3 ~/
 cp -ir .wallpapers ~/
 cp -ir .fonts ~/
+cp -ir .gtkrc-2.0 ~/
+cp -ir .bashrc ~/
+cp -ir .emacs ~/
+mkdir -p /home/alex/.config/gtk-3.0
+cp -ir settings.ini /home/alex/.config/gtk-3.0
+
 #install manually
 
 #playerctl
